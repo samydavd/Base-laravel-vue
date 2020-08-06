6641961f39043afa4a161e633f25b89cf681f842
samydavd <samydavd@gmail.com>
2020-08-05 20:14:20 -0400

    Archivos base proyecto

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+   return redirect()->action('Auth\LoginController@showLoginForm');
+});
+
+// RUTAS AUTENTICACION
+Route::namespace('Auth')->group(function () {
+	Route::get('login', 'LoginController@showLoginForm');
+	Route::post('login', 'LoginController@login')->name('login');
+	Route::get('logout', 'LoginController@logout')->name('logout');    
+});
+
+//Auth::routes();
+
+// RUTAS CON PERMISOS
+
+Route::middleware('auth')->group(function () {
+	Route::get('/home', 'HomeController@index')->name('home');
+});
